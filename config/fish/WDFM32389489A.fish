763099579a0c5497867b860e546374fd1bc3b33f
Michael Schmidt <michael.j.schmidt@gmail.com>
2014-11-20 10:08:47 +0100

    add paths to qt and postgres

--- config/fish/WDFM32389489A.fish
+++ config/fish/WDFM32389489A.fish
@@ -3,3 +3,6 @@ set -x https_proxy http://proxy.wdf.sap.corp:8080
 set -x ftp_proxy http://proxy.wdf.sap.corp:8080
 set -x all_proxy http://proxy.wdf.sap.corp:8080
 set -x no_proxy "sap.corp,localhost"
+
+set PATH /Applications/Postgres.app/Contents/Versions/9.3/bin/ $PATH
+set PATH /usr/local/Cellar/qt5/5.3.2/bin/ $PATH
