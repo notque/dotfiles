5a6ad962c40ad34bce608c4008b337dbcaba881d
Michael Schmidt <michael.j.schmidt@gmail.com>
2017-07-17 15:53:11 +0200

    updated prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,14 +1,4 @@
-function _git_branch_name
-  if command git rev-parse --git-dir > /dev/null ^ /dev/null
-    echo (git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
-  end
-end
-
-function _is_git_dirty
-  echo (git status -s --ignore-submodules=dirty ^/dev/null)
-end
-
-function _kubectl_context
+function __kubectl_context
   set -l context ""
   set -l namespace ""
 
@@ -24,39 +14,12 @@ function _kubectl_context
     set namespace "/$KUBECTL_NAMESPACE"
   end
 
-  echo $context$namespace
+  echo (set_color -o blue)‹$context$namespace›
 end
 
 function fish_prompt
-  set -l cyan (set_color -o cyan)
-  set -l yellow (set_color -o yellow)
-  set -l red (set_color -o red)
-  set -l blue (set_color -o blue)
-  set -l normal (set_color normal)
+  set -gx __fish_git_prompt_showdirtystate true
+  set -gx __fish_git_prompt_showcolorhints true
 
-  set -l arrow "$red➜ "
-  set -l cwd $cyan(basename (prompt_pwd))
-
-  set kubectl_context (_kubectl_context)
-
-  if [ $kubectl_context ]
-    set kubectl_info " $blue‹$kubectl_context›"
-  end
-
-  set git_branch_name (_git_branch_name)
-  if [ $git_branch_name ]
-    set git_info "$yellow$git_branch_name"
-    set is_git_dirty    (_is_git_dirty) 
-
-    if [ $is_git_dirty ]
-      set indicator "$red ✗"
-    else
-      set indicator "$green ✔"
-    end
-
-    set git_info " $yellow‹$git_info$indicator$yellow›"
-  end
-
-  echo -n -s $arrow $cwd $kubectl_info $git_info $normal ' '
+  echo -n (set_color -o red)➜ (set_color -o cyan)(basename (prompt_pwd)) (__fish_git_prompt) (__kubectl_context) (set_color normal) 
 end
-
