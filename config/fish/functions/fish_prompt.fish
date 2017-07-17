29979b9096ba8609cc4895555722d1ad0973ff2e
Michael Schmidt <michael.j.schmidt@gmail.com>
2017-07-17 15:32:40 +0200

    new prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -8,24 +8,23 @@ function _is_git_dirty
   echo (git status -s --ignore-submodules=dirty ^/dev/null)
 end
 
-function _rbenv_version
-  if type -P rbenv >/dev/null
-    if [ (rbenv version-origin | grep $PWD) ]
-      echo (rbenv version-name | sed -e 's/ .*//')
+function _kubectl_context
+  set -l context ""
+  set -l namespace ""
+
+  if test -n "$KUBECTL_CONTEXT"
+    set context $KUBECTL_CONTEXT
+  else
+    if type -p monsoonctl > /dev/null
+      set context (monsoonctl config current-context)
     end
   end
-end
 
-function _kubectl_context
-  set -l kubectl_namespace ""
-
-  if test -n "$KUBENAMESPACE"
-    set kubectl_namespace "/$KUBENAMESPACE"
+  if test -n "$KUBECTL_NAMESPACE"
+    set namespace "/$KUBECTL_NAMESPACE"
   end
 
-  if type -p monsoonctl > /dev/null
-    echo (monsoonctl config view -o template --template='{{index . "current-context"}}')$kubectl_namespace
-  end
+  echo $context$namespace
 end
 
 function fish_prompt
@@ -38,12 +37,8 @@ function fish_prompt
   set -l arrow "$red➜ "
   set -l cwd $cyan(basename (prompt_pwd))
 
-  set rbenv_version (_rbenv_version)
-  if [ $rbenv_version ]
-    set ruby_info " $red‹$rbenv_version›"
-  end
- 
   set kubectl_context (_kubectl_context)
+
   if [ $kubectl_context ]
     set kubectl_info " $blue‹$kubectl_context›"
   end
@@ -51,8 +46,9 @@ function fish_prompt
   set git_branch_name (_git_branch_name)
   if [ $git_branch_name ]
     set git_info "$yellow$git_branch_name"
+    set is_git_dirty    (_is_git_dirty) 
 
-    if [ (_is_git_dirty) ]
+    if [ $is_git_dirty ]
       set indicator "$red ✗"
     else
       set indicator "$green ✔"
@@ -61,6 +57,6 @@ function fish_prompt
     set git_info " $yellow‹$git_info$indicator$yellow›"
   end
 
-  echo -n -s $arrow $cwd $ruby_info $kubectl_info $git_info $normal ' '
+  echo -n -s $arrow $cwd $kubectl_info $git_info $normal ' '
 end
 
