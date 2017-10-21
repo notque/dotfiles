646ab4d35df517fbd743e04fa176537db8ade5bf
Michael Schmidt <michael.j.schmidt@gmail.com>
2017-10-21 11:02:55 +0200

    removes symlink functionality

--- config/fish/functions/_helm.fish
+++ config/fish/functions/_helm.fish
@@ -1,5 +1,4 @@
 function _helm
-  set -l cmd helm 
-  test -n "$KUBECTL_CONTEXT"; or set -x KUBECTL_CONTEXT (monsoonctl config current-context)
-  eval $cmd --kube-context $KUBECTL_CONTEXT $argv
+  test -n "$KUBECTL_CONTEXT"; or set -x KUBECTL_CONTEXT (kubectl config current-context)
+  eval helm --kube-context $KUBECTL_CONTEXT $argv
 end
