9aec73d1a69718235e691bf350158611617f5b9e
Michael Schmidt <michael.j.schmidt@gmail.com>
2017-10-21 10:56:28 +0200

    adds kubernikus reauth

--- config/fish/functions/_kubectl.fish
+++ config/fish/functions/_kubectl.fish
@@ -1,11 +1,13 @@
 function _kubectl 
-  set -l cmd monsoonctl
-  test -n "$KUBECTL_CONTEXT"; or set -x KUBECTL_CONTEXT (monsoonctl config current-context)
-  test "$KUBECTL_CONTEXT" = "wupse"; and set cmd kubectl.1.7.0
+  test -n "$KUBECTL_CONTEXT"; or set -x KUBECTL_CONTEXT (kubectl config current-context)
+
+  if count $argv > /dev/null
+    kubernikusctl auth refresh --context $KUBECTL_CONTEXT
+  end
 
   if test -n "$KUBECTL_NAMESPACE"
-    eval $cmd --context $KUBECTL_CONTEXT --namespace $KUBECTL_NAMESPACE $argv
+    eval kubectl --context $KUBECTL_CONTEXT --namespace $KUBECTL_NAMESPACE $argv
   else
-    eval $cmd --context $KUBECTL_CONTEXT $argv
+    eval kubectl --context $KUBECTL_CONTEXT $argv
   end
 end
